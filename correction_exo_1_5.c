90e75902402fc256a5df56b395677f8aebf999f2
yzpt <yzpt.de@gmail.com>
2025-01-08 15:50:27 +0100

    update

--- correction_exo_1_5.c
+++ correction_exo_1_5.c
@@ -18,7 +18,23 @@ int main() {
     // On utilise un tableau prédéfini plutôt que de réclamer à l'utilisateur de rentrer 10 nombres
     int tableau[10] = {56, 12, 3, 4, 99, 23, 45, 67, 89, 101};
 
-    
+    // Affichage des nombres pairs
+    printf("Nombres pairs: ");
+    for (int i = 0; i < 10; i++) {
+        if (tableau[i] % 2 == 0) {
+            printf("%d ", tableau[i]);
+        }
+    }
+    printf("\n");
+
+    // Affichage des nombres impairs
+    printf("Nombres impairs: ");
+    for (int i = 0; i < 10; i++) {
+        if (tableau[i] % 2 != 0) {
+            printf("%d ", tableau[i]);
+        }
+    }
+    printf("\n");
 
 
     return 0;
