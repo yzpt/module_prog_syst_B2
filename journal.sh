bb2cc772ca7ff282dd470367d8dbe3c75535c901
yzpt <yzpt.de@gmail.com>
2025-01-08 10:04:08 +0100

    update readme

--- journal.sh
+++ journal.sh
@@ -1,11 +1,17 @@
 sudo apt-get install build-essential
 sudo apt-get install gcc
 
+sudo apt update 
+
+
 gcc exercice.c -o exercice
 ./exercice
 
-# gh create repo module_prog_systeme private
-gh repo create module_prog_systeme --private
+# gh create repo module_prog_syst_B2 private
+gh repo create module_prog_syst_B2 --public
+
+#gh repo delete
+gh repo delete yzpt/module_prog_systeme --confirm
 
 # delete .git
 rm -rf .git -f
@@ -13,14 +19,16 @@ rm -rf .git -f
 
 git init
 git branch -M main
-git remote add origin https://github.com/yzpt/module_prog_systeme.git
+git remote add origin https://github.com/yzpt/module_prog_syst_B2.git
 git add .
 git commit -m "exos & orga"
 git push -u origin main
 # git push -u origin main --force
 
 # github set public
-gh repo edit yzpt/module_prog_systeme --visibility public --accept-visibility-change-consequences 
+gh repo edit yzpt/module_prog_syst_B2 --visibility public --accept-visibility-change-consequences 
 
 # github repo set private
-gh repo edit yzpt/module_prog_systeme --visibility private --accept-visibility-change-consequences
+gh repo edit yzpt/module_prog_syst_B2 --visibility private --accept-visibility-change-consequences
+
+
--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
-exos à changer :
-1.8
-3.1
-3.3
+# B2-Cyber - Programmation système
 
 
+## Mercredi 8 janvier 2025
 
+### [2.0.exos_progressifs.md](2.0.exos_progressifs.md) 
 
+### Soyez proactifs dans votre apprentissage !
+- **RECHERCHEZ** par vous-même les algorithmes, c'est très important, essence de l'apprentissage !
+- En cas de blocage/difficultés: consultez dans l'ordre :
+  - Moi (l'enseignant)
+  - ChatGPT / Google
+  - La correction
+  
