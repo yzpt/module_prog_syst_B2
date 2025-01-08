efec51fe61ca99f9922f2484b0a79ff2a445934c
yzpt <yzpt.de@gmail.com>
2025-01-08 15:09:05 +0100

    update

---
+++ correction_exo_1_3.c
@@ -0,0 +1,22 @@
+/*
+- Dmandez à l'utilisateur d'entrer 10 nombres --> utiliser un tableau prédéfini.
+- Demandez ensuite un nombre à rechercher.
+- Comptez combien de fois ce nombre apparaît dans le tableau et affichez le résultat.
+
+*/ 
+
+
+#include <stdio.h>
+
+int main() {
+
+    // On utilise un tableau prédéfini plutôt que de réclamer à l'utilisateur de rentrer 10 nombres
+    int tableau[10] = {56, 12, 6, 56, 8, 56, 12, 56, 56, 56};
+
+    
+
+
+    return 0;
+}
+
+
