eef0bb87e9a62763e483a245b74dcf3e6819a98b
yzpt <yzpt.de@gmail.com>
2025-01-10 14:20:43 +0100

    update

--- repos_etudiants.sh
+++ repos_etudiants.sh
@@ -2,32 +2,26 @@ cd repos_etudiants
 
 
 # Mareme
-cd ..
 rm -rf mareme && git clone https://github.com/Mareme25/repo_Mareme.git mareme
 cd mareme && git log && cd ..
 
 # Martin
-cd ..
-git clone https://github.com/HEMELSDAEL/prog_systeme.git martin
+git clone https://github.com/strandw/exos-C.git martin
 cd martin && git log && cd ..
 
 # Macilia
-cd ..
 rm -rf macilia && git clone https://github.com/Macycola/MacyEtYohann.git macilia
 cd macilia && git log && cd ..
 
 # Angel
-cd ..
 git clone https://github.com/juanangel77/exercices_c.git angel
 cd angel && git log && cd ..
 
 # Alexandre
-cd ..
 rm -rf alexandre && git clone https://github.com/HEMELSDAEL/prog_systeme.git alexandre
 cd alexandre && git log && cd ..
 
 # Yanis
-
 rm -rf yanis && git clone https://github.com/yanisOuali21/starting-c.git yanis
 cd yanis && git log && cd ..
 
@@ -36,6 +30,5 @@ rm -rf franck && git clone https://github.com/fao241/Tableaux-et-pointeurs.0.git
 cd franck && git log && cd ..
 
 # Emmanuel
-cd ..
 rm -rf emmanuel && git clone https://github.com/Hendrix44/Exercice_c.git emmanuel
 cd emmanuel && git log && cd ..
