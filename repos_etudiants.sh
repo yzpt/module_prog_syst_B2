6fc28d34304dfae6a78aeb33e4e64c4f329db537
yzpt <yzpt.de@gmail.com>
2025-01-10 10:06:13 +0100

    update

---
+++ repos_etudiants.sh
@@ -0,0 +1,34 @@
+cd repos_etudiants
+
+
+# Mareme
+rm -rf mareme && git clone https://github.com/Mareme25/repo_Mareme.git mareme
+cd mareme && git log && cd ..
+
+# Martin
+git clone https://github.com/HEMELSDAEL/prog_systeme.git martin
+cd martin && git log && cd ..
+
+# Macilia
+git clone https://github.com/Macycola/MacyEtYohann.git macilia
+cd macilia && git log && cd ..
+
+# Angel
+git clone https://github.com/juanangel77/exercices_c.git angel
+cd angel && git log && cd ..
+
+# Alexandre
+rm -rf alexandre && git clone https://github.com/HEMELSDAEL/prog_systeme.git alexandre
+cd alexandre && git log && cd ..
+
+# Yanis
+rm -rf yanis && git clone https://github.com/yanisOuali21/starting-c.git yanis
+cd yanis && git log && cd ..
+
+# Franck
+rm -rf franck && git clone
+cd franck && git log && cd ..
+
+# Emmanuel
+rm -rf emmanuel && git clone
+cd emmanuel && git log && cd ..
